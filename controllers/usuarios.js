105617df0c2618aaab04480517adfd62eb5a8c57
LucianaGallardoP <lgallardop02@gmail.com>
2026-04-04 15:57:40 -0300

    Empezar a crear archivo server. Crear archivo config.js (conectar BD). Crear archivo usuarios.js en carpeta routes (definir ruta usuariosPost). Crear archivo controlador de ruta usuarios con verbo Post.

---
+++ controllers/usuarios.js
@@ -0,0 +1,31 @@
+const { response, request } = require("express");
+const bcrypt = require("bcryptjs");
+const Usuario = require("../models/usuario");
+
+const usuarioPost = async (req = request, res = response) => {
+  // Recibir el cuerpo de la peticion
+  const datos = req.body;
+
+  const { nombre, apellido, correo, password, rol } = datos;
+  const usuario = new Usuario({
+    nombre,
+    apellido,
+    correo,
+    password,
+    rol,
+  });
+
+  //   Encriptar la contraseña
+
+  // Guardar los datos en la BD
+  await usuario.save();
+
+  res.json({
+    mensaje: "Usuario cargado correctamente en la BD",
+    usuario,
+  });
+};
+
+module.exports = {
+  usuarioPost,
+};
---
+++ models/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const { dbConnection } = require("../database/config");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 3000;
+    this.authPath = "/api/auth";
+    this.usuariosPath = "/api/usuarios";
+    this.categoriasPath = "/api/categorias";
+    this.productosPath = "/api/productos";
+    this.ventasDiaPath = "/api/ventasDia";
+    this.ventasMesPath = "/api/ventasMes";
+
+    // Conectar con BD
+    this.conectarDB();
+
+    // Middlewares
+    this.middlewares();
+
+    // Funcion para rutas
+    this.routes();
+  }
+
+  async conectarDB() {
+    await dbConnection();
+  }
+
+  middlewares() {
+    // CORS
+    this.app.use(cors());
+    // Leer lo que el usuario envia desde el front end
+    this.app.use(express.json());
+    // Definir una carpeta public
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    this.app.use(this.usuariosPath, require('../routes/usuarios'))
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Server online in port: ", this.port);
+    });
+  }
+}
+
+module.exports = Server;
